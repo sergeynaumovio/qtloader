17518b906597e424c15aa4c04a7ace240e066577
Sergey Naumov <sergey@naumov.io>
2022-02-03 01:00:26 +0300

    REF: core: qt6 debug included by default

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2021 Sergey Naumov
+** Copyright (C) 2021, 2022 Sergey Naumov
 **
 ** Permission to use, copy, modify, and/or distribute this
 ** software for any purpose with or without fee is hereby granted.
@@ -21,7 +21,6 @@
 #include <QFileDialog>
 #include <QLoaderTree>
 #include <QMessageBox>
-#include <QDebug>
 
 namespace {
 
