bc0ee9ff8b7f973a15b302fd744d0659e863370a
Sergey Naumov <sergey@naumov.io>
2022-03-26 10:34:28 +0300

    NEW: core: icon interface

---
+++ src/core/qloadericoninterface.h
@@ -0,0 +1,31 @@
+/****************************************************************************
+**
+** Copyright (C) 2022 Sergey Naumov
+**
+** Permission to use, copy, modify, and/or distribute this
+** software for any purpose with or without fee is hereby granted.
+**
+** THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
+** THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+** CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+** LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+** NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+** CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+**
+****************************************************************************/
+
+#ifndef QLOADERICONINTERFACE_H
+#define QLOADERICONINTERFACE_H
+
+#include <QIcon>
+
+class QLoaderIconInterface
+{
+public:
+    virtual QIcon icon() const = 0;
+};
+Q_DECLARE_INTERFACE(QLoaderIconInterface, "QLoaderIconInterface")
+
+#endif // QLOADERICONINTERFACE_H
