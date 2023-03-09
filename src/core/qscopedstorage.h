18ac997ba4d234bb152dbd4bcd5a3e1634d5b98a
Sergey Naumov <sergey@naumov.io>
2023-03-09 16:50:36 +0300

    NEW: core: scoped storage

---
+++ src/core/qscopedstorage.h
@@ -0,0 +1,39 @@
+// Copyright (C) 2023 Sergey Naumov <sergey@naumov.io>
+// SPDX-License-Identifier: 0BSD
+
+#ifndef QSCOPEDSTORAGE_H
+#define QSCOPEDSTORAGE_H
+
+#include <QtCore/qglobal.h>
+
+template<typename T, int Size, int Alignment = sizeof(ptrdiff_t)>
+class QScopedStorage
+{
+    Q_DISABLE_COPY_MOVE(QScopedStorage)
+
+    std::aligned_storage_t<Size, Alignment> d_storage;
+
+    template<int TSize, int TAlignment>
+    static void checkSize() noexcept
+    {
+        static_assert(Size == TSize, "sizeof(QScopedStorage) and sizeof(T) mismatch");
+        static_assert(Alignment == TAlignment, "alignof(QScopedStorage) and alignof(T) mismatch");
+    }
+
+    T *d_func() const noexcept { return reinterpret_cast<T *>((void *)(&d_storage)); }
+
+public:
+    template<typename... Args>
+    explicit QScopedStorage(Args&&... args) { new (d_func()) T(std::forward<Args>(args)...); }
+
+    ~QScopedStorage() noexcept
+    {
+        d_func()->~T();
+        checkSize<sizeof(T), alignof(T)>();
+    }
+
+    T *operator->() const noexcept { return d_func(); }
+    T &operator*() const noexcept { return *d_func(); }
+};
+
+#endif // QSCOPEDSTORAGE_H
